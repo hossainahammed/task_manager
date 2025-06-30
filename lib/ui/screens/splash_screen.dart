1e2137f64e8cd9eeadd80721a914d2187b179593
hossainahammed <hossainahammed627@gmail.com>
2025-07-01 01:38:50 +0600

    flutter svg folder add

---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class splashScreen extends StatefulWidget {
+  const splashScreen({super.key});
+
+  @override
+  State<splashScreen> createState() => _splashScreenState();
+}
+
+class _splashScreenState extends State<splashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SvgPicture.asset('assets/image/logo.svg')
+        ],
+
+
+      ),
+    );
+  }
+}
