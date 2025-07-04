7b7cd20026f274f1a8e043b289ee6b9476b7624c
hossainahammed <hossainahammed627@gmail.com>
2025-07-04 16:13:17 +0600

    Do it scrollable(Adaptisness)

--- lib/ui/screens/sign_in_screen.dart
+++ lib/ui/screens/sign_in_screen.dart
@@ -16,66 +16,68 @@ class _SignInScreenState extends State<SignInScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ScreenBagground(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 80),
-              Text(
-                'Get Started With',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              SizedBox(height: 24),
-              TextFormField(
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(hintText: 'Email'),
-              ),
-              SizedBox(height: 8),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(hintText: 'Password'),
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: _onTapSigninButton,
-                child: Icon(Icons.arrow_circle_right_outlined),
-              ),
-              SizedBox(height: 32),
-              Center(
-                child: Column(
-                  children: [
-                    TextButton(
-                      onPressed: _onTapForgotPasswordButton,
-                      child: Text(
-                        'Forget password ?',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ),
-                    RichText(
-                      text: TextSpan(
-                        text: "Don't have any account ?",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          letterSpacing: 0.4,
-                        ),
-                        children: [
-                          TextSpan(
-                            text: 'Sign Up',
-                            style: TextStyle(
-                              color: Colors.green,
-                              fontWeight: FontWeight.w700,
-                            ),
-                            recognizer:
-                                TapGestureRecognizer()..onTap = _onTapSignUpButton,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 80),
+                Text(
+                  'Get Started With',
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
-              ),
-            ],
+                SizedBox(height: 24),
+                TextFormField(
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(hintText: 'Email'),
+                ),
+                SizedBox(height: 8),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(hintText: 'Password'),
+                ),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: _onTapSigninButton,
+                  child: Icon(Icons.arrow_circle_right_outlined),
+                ),
+                SizedBox(height: 32),
+                Center(
+                  child: Column(
+                    children: [
+                      TextButton(
+                        onPressed: _onTapForgotPasswordButton,
+                        child: Text(
+                          'Forget password ?',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          text: "Don't have any account ?",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 0.4,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: 'Sign Up',
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.w700,
+                              ),
+                              recognizer:
+                                  TapGestureRecognizer()..onTap = _onTapSignUpButton,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
