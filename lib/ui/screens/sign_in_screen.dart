8c5f2557b9582e13cb1be1371c1c92f4f134b31a
hossainahammed <hossainahammed627@gmail.com>
2025-07-04 16:10:02 +0600

    Give style(Do it center )for  Rich text for don't have account and sign up  text

--- lib/ui/screens/sign_in_screen.dart
+++ lib/ui/screens/sign_in_screen.dart
@@ -21,47 +21,62 @@ class _SignInScreenState extends State<SignInScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 80,),
-              Text('Get Started With',style: Theme.of(context).textTheme.titleLarge,),
-              SizedBox(height: 24,),
+              SizedBox(height: 80),
+              Text(
+                'Get Started With',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              SizedBox(height: 24),
               TextFormField(
                 textInputAction: TextInputAction.next,
-                decoration:InputDecoration(
-                  hintText: 'Email',
-                ) ,
+                decoration: InputDecoration(hintText: 'Email'),
               ),
-              SizedBox(height: 8,),
+              SizedBox(height: 8),
               TextFormField(
                 obscureText: true,
-                decoration:InputDecoration(
-                  hintText: 'Password',
-
-                ) ,
+                decoration: InputDecoration(hintText: 'Password'),
               ),
-              SizedBox(height: 16,),
+              SizedBox(height: 16),
               ElevatedButton(
                 onPressed: _onTapSigninButton,
                 child: Icon(Icons.arrow_circle_right_outlined),
               ),
-              SizedBox(height: 32,),
-              TextButton(onPressed: _onTapForgotPasswordButton, child: Text('Forget password ?',style: TextStyle(color: Colors.grey),)),
-
-              RichText(text: TextSpan(text: "Don't have any account ?",
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  letterSpacing: 0.4
-                ),
-                children: [
-                  TextSpan(
-                    text: 'Sign Up',
-                    style: TextStyle(
-                      color: Colors.green,
-                      fontWeight: FontWeight.w700
+       Center(
+         child: Column(
+                  children: [
+                    TextButton(
+                      onPressed: _onTapForgotPasswordButton,
+                      child: Text(
+                        'Forget password ?',
+                        style: TextStyle(color: Colors.grey),
+                      ),
                     ),
-                    recognizer: TapGestureRecognizer()..onTap = _onTapSignUpButton
-                  ),
-                ]
-              ))
+                    RichText(
+                      text: TextSpan(
+                        text: "Don't have any account ?",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 0.4,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: 'Sign Up',
+                            style: TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.w700,
+                            ),
+                            recognizer:
+                                TapGestureRecognizer()..onTap = _onTapSignUpButton,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+       )         ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
@@ -69,14 +84,7 @@ class _SignInScreenState extends State<SignInScreen> {
     );
   }
 
-  void _onTapSigninButton(){
-
-  }
-  void _onTapForgotPasswordButton(){
-
-  }
-  void _onTapSignUpButton(){
-
-  }
-
+  void _onTapSigninButton() {}
+  void _onTapForgotPasswordButton() {}
+  void _onTapSignUpButton() {}
 }
