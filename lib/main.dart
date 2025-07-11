55eba89e8cdb5a0a9b25337868c8ae46ec463da5
hossainahammed <hossainahammed627@gmail.com>
2025-07-11 23:15:19 +0600

    Add Photo picker to  Update Profile  screen ui design

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:device_preview/device_preview.dart';
 import 'app.dart';
 void main(){
   //runApp(TaskManagerApp());
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
       DevicePreview(
         enabled: true,
