9a30d462be79d389c6f2f289feebef881a892350
hossainahammed <hossainahammed627@gmail.com>
2025-07-12 23:30:32 +0600

    Do Process for add Network caller

---
+++ lib/data/service/network_caller.dart
@@ -0,0 +1,6 @@
+// class NetworkCaller{
+//   static Future<Response> getRequest({required String url}) async{
+//     Uri uri = uri.parse(url);
+//     Response response = await get (uri);
+//   }
+// }
