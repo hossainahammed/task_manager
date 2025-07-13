f93847c83edbcc7e080b29dbe305b644faae8509
hossainahammed <hossainahammed627@gmail.com>
2025-07-13 10:58:32 +0600

    Do Process for add Network caller

--- lib/data/service/network_caller.dart
+++ lib/data/service/network_caller.dart
@@ -1,6 +1,28 @@
-// class NetworkCaller{
-//   static Future<Response> getRequest({required String url}) async{
-//     Uri uri = uri.parse(url);
-//     Response response = await get (uri);
-//   }
-// }
+import 'package:http/http.dart';
+
+class NetworkResponse {
+  final bool isSuccess;
+  final int statusCode;
+  final Map<String, dynamic>? body;
+  final String? errorMessage;
+
+  NetworkResponse({
+    required this.isSuccess,
+    required this.statusCode,
+    this.body,
+    this.errorMessage,
+  });
+}
+
+class NetworkCaller {
+  static Future<Response> getRequest({required String url}) async {
+    Uri uri = Uri.parse(url);
+    Response response = await get(uri);
+    if (response.statusCode == 200) {
+      // Handle success
+    } else {
+      // Handle error
+    }
+    return response; // Make sure to return the response
+  }
+}
