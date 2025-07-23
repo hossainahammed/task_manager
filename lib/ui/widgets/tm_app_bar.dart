220a412d899a28c8b17e9407a971a43b4a29164c
hossainahammed <hossainahammed627@gmail.com>
2025-07-23 13:44:36 +0600

    Add appbar Profile picture

--- lib/ui/widgets/tm_app_bar.dart
+++ lib/ui/widgets/tm_app_bar.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import 'package:task_manager/ui/controllers/auth_controller.dart';
@@ -5,16 +7,14 @@ import 'package:task_manager/ui/screens/sign_in_screen.dart';
 import 'package:task_manager/ui/screens/update_profile_screen.dart';
 
 class TaskManagerAppBar extends StatefulWidget implements PreferredSizeWidget {
-  const TaskManagerAppBar({
-    super.key,
-  });
+  const TaskManagerAppBar({super.key});
 
   @override
   State<TaskManagerAppBar> createState() => _TaskManagerAppBarState();
 
   @override
   // TODO: implement preferredSize
-  Size get preferredSize =>Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
 
 class _TaskManagerAppBarState extends State<TaskManagerAppBar> {
@@ -28,7 +28,13 @@ class _TaskManagerAppBarState extends State<TaskManagerAppBar> {
           children: [
             CircleAvatar(
               radius: 20.0,
-              backgroundImage:NetworkImage('https://scontent.fdac198-1.fna.fbcdn.net/v/t39.30808-6/480468323_2092966654471743_3439343231728926675_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFMUI1Y_brqm7yt7Ubs-OiO2jIFX1frtODaMgVfV-u04GhkY7LHt7hZbwly0eoLFmXxHLoQfvlZN48Whxz6mRqQ&_nc_ohc=55TYNfi1faIQ7kNvwGhDMnV&_nc_oc=AdnrflqU5gR8xclBl3ajBoABqbWKBftbWHqksn2harCcp6_vTEBJ3I1eVMESCxHIKdk&_nc_zt=23&_nc_ht=scontent.fdac198-1.fna&_nc_gid=ciNP6IVWFiqUmxlDUi4y6w&oh=00_AfRXhS-Ko4ajDCXHlanX7vjY9_MfODwJM_TVDmUh5__hhA&oe=6871B4D2'),
+              // backgroundImage:NetworkImage('https://scontent.fdac198-1.fna.fbcdn.net/v/t39.30808-6/480468323_2092966654471743_3439343231728926675_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFMUI1Y_brqm7yt7Ubs-OiO2jIFX1frtODaMgVfV-u04GhkY7LHt7hZbwly0eoLFmXxHLoQfvlZN48Whxz6mRqQ&_nc_ohc=55TYNfi1faIQ7kNvwGhDMnV&_nc_oc=AdnrflqU5gR8xclBl3ajBoABqbWKBftbWHqksn2harCcp6_vTEBJ3I1eVMESCxHIKdk&_nc_zt=23&_nc_ht=scontent.fdac198-1.fna&_nc_gid=ciNP6IVWFiqUmxlDUi4y6w&oh=00_AfRXhS-Ko4ajDCXHlanX7vjY9_MfODwJM_TVDmUh5__hhA&oe=6871B4D2'),
+              backgroundImage:
+                  AuthController.userModel?.photo == null
+                      ? null
+                      : MemoryImage(
+                        base64Decode(AuthController.userModel?.photo ?? ''),
+                      ),
             ),
             SizedBox(width: 16),
             Expanded(
@@ -36,7 +42,7 @@ class _TaskManagerAppBarState extends State<TaskManagerAppBar> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                   AuthController.userModel!.fullName ,
+                    AuthController.userModel!.fullName,
                     style: TextStyle(
                       fontSize: 14,
                       fontWeight: FontWeight.w500,
@@ -54,32 +60,36 @@ class _TaskManagerAppBarState extends State<TaskManagerAppBar> {
                 ],
               ),
             ),
-            IconButton(onPressed:_onTapLogoutButton, icon: Icon(Icons.logout_outlined)),
+            IconButton(
+              onPressed: _onTapLogoutButton,
+              icon: Icon(Icons.logout_outlined),
+            ),
           ],
         ),
       ),
     );
   }
 
-Future<void> _onTapLogoutButton() async{
-   await AuthController.clearData();
- Navigator.pushNamedAndRemoveUntil(context, SignInScreen.name, (predicate)=>false);
-}
+  Future<void> _onTapLogoutButton() async {
+    await AuthController.clearData();
+    Navigator.pushNamedAndRemoveUntil(
+      context,
+      SignInScreen.name,
+      (predicate) => false,
+    );
+  }
 
-  void _onTapProfileBar(){
-    if(ModalRoute.of(context)!.settings.name != UpdateProfileScreen.name){
+  void _onTapProfileBar() {
+    if (ModalRoute.of(context)!.settings.name != UpdateProfileScreen.name) {
       Navigator.pushNamed(context, UpdateProfileScreen.name);
     }
-
-
   }
-                // OR (We can also do it by this )
-// void _onTapProfileBar(){
-//     if(ModalRoute.of(context)!.settings.name == UpdateProfileScreen.name){
-//       return;
-//     }
-//
-//     Navigator.pushNamed(context, UpdateProfileScreen.name);
-// }
-
+  // OR (We can also do it by this )
+  // void _onTapProfileBar(){
+  //     if(ModalRoute.of(context)!.settings.name == UpdateProfileScreen.name){
+  //       return;
+  //     }
+  //
+  //     Navigator.pushNamed(context, UpdateProfileScreen.name);
+  // }
 }
